f4cfe9d7614d1c4342891394ddba8a6afd3a293c
s.hamada <hamasho1994@icloud.com>
2020-12-21 08:36:09 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name' , 'email', 'password'
     ];
 
     /**
@@ -36,4 +36,70 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'user_follow','user_id','follow_id')->withTimestamps();
+        
+    }
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'user_follow','follow_id','user_id')->withTimestamps();
+    }
+                 // loadRelationshipCounts
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts','followings','followers');
+    }
+    
+    public function follow($userId)
+    {
+        //すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        //対象は自分自身かどうかの確認
+        $its_me =$this->id == $userId;
+        
+        if($exist|| $its_me) {
+            //すでにフォローしていれば何もしない
+            return false;
+        }else {
+            //未フォローであればフォローする
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    public function unfollow($userId)
+    {
+        //すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        //対象が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+        
+        if ($exist && !$its_me) {
+            //すでにフォロしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            //未フォローであれば何もしない
+            return false;
+        }
+    }
+    
+    public function is_following($userId)
+    {
+        return $this->followings()->where('follow_id',$userId)->exists();
+    }
+    
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
